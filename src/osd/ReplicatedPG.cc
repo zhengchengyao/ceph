cb79abd53ce7aa436e67b5fc7875b1daedc1838a
J. Eric Ivancich <ivancich@redhat.com>
2016-06-17 12:40:30 -0400

    Make constructor for PGQueueable explicit to make references to that specific constructor easier to find. Was difficult With the implicit constructor invokation plus use of make_pair.

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -8425,7 +8425,7 @@ void ReplicatedPG::op_applied(const eversion_t &applied_version)
 	osd->op_wq.queue(
 	  make_pair(
 	    this,
-	    scrubber.active_rep_scrub));
+	    PGQueueable(scrubber.active_rep_scrub)));
 	scrubber.active_rep_scrub = OpRequestRef();
       }
     }
@@ -9651,7 +9651,7 @@ void ReplicatedPG::_applied_recovered_object_replica()
     osd->op_wq.queue(
       make_pair(
 	this,
-	scrubber.active_rep_scrub));
+	PGQueueable(scrubber.active_rep_scrub)));
     scrubber.active_rep_scrub = OpRequestRef();
   }
 
